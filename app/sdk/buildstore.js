71c96807df6ec260d68e4e7152186a2a417db96f
paybackman <travis.plummer@hp.com>
2014-09-18 16:57:15 -0500

    adding Buildstore

---
+++ app/sdk/buildstore.js
@@ -0,0 +1,41 @@
+﻿var klass = require('klass')
+  , _ = require('underscore')
+  , request = require('request');
+
+var BuildStore = module.exports = klass(function () {
+    // constructor
+
+}).methods({    
+    
+    getBuildsForUser : function (repoId, done) {
+
+        var username = 'stackato'
+        var password = 'asdf'
+        var options = {
+                        rejectUnauthorized: false,
+                        url: 'http://inabox.15.126.228.197.xip.io/jobs/' + repoId + '/builds/',
+                        auth:
+                          {
+                            user: username,
+                            password: password
+                          }
+                       }
+
+        request(options, function (err, res, body) {
+        if (err) {
+        console.dir(err)                
+        }
+            console.dir('headers', res.headers)
+            console.dir('status code', res.statusCode)
+            console.dir(body)
+            done(body);
+        })
+
+    },
+    
+    getBuild : function (buildId, done) {
+        //var storedRepo = _.find(this.repos, function (repo) { return repo.id === repoId });
+        //debugger;
+        //done(storedRepo)
+    }
+});
